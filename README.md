2656cabfacb65a23b6d77cdfeda3125e96c6f8f6
Reese Wilson <reese@shinymayhem.com>
2015-03-19 23:27:32 +0000

    basic incrementer works with instance-level persistence
    still needs cloud persistence option

--- README.md
+++ README.md
@@ -1,18 +1,49 @@
-Docker microservice for persistent incrementing
+Docker microservice component for persistent incrementing. Capable of handling multiple simultaneous requests without collisions (i.e. no two requests should ever receive the same result because of timing).
 
 ##Usage
+GET request increment by 1. POST requests increment by the number specified in the URL. PUT requests set the incrementer to the value specified in the URL. Namespace is optional.
+
+Requires connection to redis. Currently uses default port, no password, `incrementer-redis` host. See the following .yml file for example configuration:
+
+```yml
+incrementer-api:
+    image: shinymayhem/micro-incrementer
+    ports:
+        - "80"
+    volumes_from:
+        - incrementer-redis #for persistence after instance destroy
+    links:
+        - incrementer-redis #format- service:alias
+incrementer-redis:
+    image: tutum/redis
+    ports:
+        - "6379"
+    environment:
+        REDIS_PASS: "**None**"
+        REDIS_APPENDONLY: yes
+        REDIS_APPENDFSYNC: always
+        #REDIS_APPENDFSYNC: everysec #for better performance
+```
+
+##Examples
+
 Get new incremented number in default namespace
 
-`GET /`
+`GET /` returns 1
+
+`GET /` returns 2
 
 Get new incremented number in `myapp` namespace
 
-`GET /myapp`
+`GET /myapp` returns 1
+
+`GET /` returns 3
 
 Get number after 200 increments to `myapp` namespace
 
-`POST /myapp/200`
+`POST /myapp/200` returns 202
 
 Set incrementer to 300. Next returned number will be 301
 
-`PUT /myapp/300`
+`PUT /myapp/300` returns {success: true}
+`GET /myapp` returns 301
--- src/index.js
+++ src/index.js
@@ -1,5 +1,107 @@
 var express = require('express');
 var app = express();
+var redis = require("redis");
+var client = redis.createClient(6379, 'incrementer-redis');
+var defaultKey = "default-key";
+
+client.on('connect', function() {
+	console.log("Connected to redis");
+});
+
+client.on('error', function(err) {
+	console.log("Redis error", err);
+});
+
+app.use(function(req, res, next) {
+	var date = new Date();
+	console.log("Got " + req.method + " request to " + req.originalUrl + " at " + date.toDateString() + " " + date.toTimeString());
+	next();
+});
+
+app.get("/", function(req, res, next) {
+	client.incr(defaultKey, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		res.json(reply);
+	});
+});
+
+app.get("/:namespace", function(req, res, next) {
+	var namespace = req.params.namespace;
+	client.incr(namespace, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		res.json(reply);
+	});
+});
+
+app.post("/:number", function(req, res, next) {
+	var number = parseInt(req.params.number);
+	if (isNaN(number)) {
+		res.status(422);
+		res.json({error: "Single URL parameter in POST request must be numeric"});
+		return;
+	}
+	client.incrby(defaultKey, number, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		res.json(reply);
+	});
+});
+
+app.post("/:namespace/:number", function(req, res, next) {
+	var namespace = req.params.namespace;
+	var number = req.params.number;
+	if (isNaN(number)) {
+		res.status(422);
+		res.json({error: "Second URL parameter in POST request must be numeric"});
+		return;
+	}
+	client.incrby(namespace, number, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		res.json(reply);
+	});
+});
+
+app.put("/:number", function(req, res, next) {
+	var number = req.params.number;
+	if (isNaN(number)) {
+		res.status(422);
+		res.json({error: "Single URL parameter in PUT request must be numeric"});
+		return;
+	}
+	client.set(defaultKey, number, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		if (reply === "OK") {
+			res.json({success: true});
+		} else {
+			res.status(500);
+			res.json({error: "Something went wrong"});
+		}
+	});
+});
+
+app.put("/:namespace/:number", function(req, res, next) {
+	var namespace = req.params.namespace;
+	var number = req.params.number;
+	if (isNaN(number)) {
+		res.status(422);
+		res.json({error: "Second URL parameter in PUT request must be numeric"});
+		return;
+	}
+	client.set(namespace, number, function(err, reply) {
+		if (err) throw(err);
+		console.log(reply);
+		if (reply === "OK") {
+			res.json({success: true});
+		} else {
+			res.status(500);
+			res.json({error: "Something went wrong"});
+		}
+	});
+});
 
 var server = app.listen(80, function() {
 	console.log("Incrementer server started on port 80");
